5889d844c1ab1d8b850765c43fb7fa35e6073b37
kamiokan <spherenov@gmail.com>
2021-05-11 09:22:28 +0900

    rubocopチェックし警告部分を修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,4 +1,5 @@
 #! /usr/bin/env ruby
+# frozen_string_literal: true
 
 # フレームごとの配列に整えるパート
 score = ARGV[0]
@@ -6,11 +7,9 @@ scores = score.split(',')
 shots = []
 scores.each do |s|
   if s == 'X' # ストライク
-    if shots.size >= 18
-      # 10フレーム目
-      shots << 10
-    else
-      shots << 10
+    shots << 10
+    if shots.size < 18
+      # 10フレーム目以外
       shots << 0
     end
   else
@@ -28,36 +27,36 @@ unless frames[10].nil?
   frames.delete_at(10)
 end
 
-#　点数計算パート
+# 点数計算パート
 point = 0
 frame_number = 1
 frames.each do |frame|
   if frame_number == 10
-    # 10ゲーム目
+    # 10フレーム目
     point += frame.sum
-  else
-    if frame[0] == 10
-      # ストライクの時の計算
-      next_frame = frames[frame_number]
-      if next_frame[0] == 10 && frame_number != 9
-        # 次のフレームもストライクで9フレーム目以外の場合
-        next_next_frame = frames[frame_number + 1]
-        point += 10 + next_frame[0] + next_next_frame[0]
-      else
-        point += 10 + next_frame[0] + next_frame[1]
-      end
-    elsif frame.sum == 10
-      # スペアの時の計算
-      next_frame = frames[frame_number]
-      point += 10 + next_frame[0]
+    next
+  end
+
+  # 1フレーム目から9フレーム目までの処理
+  if frame[0] == 10
+    # ストライクの時の計算
+    next_frame = frames[frame_number]
+    if next_frame[0] == 10 && frame_number != 9
+      # 次のフレームもストライクで9フレーム目以外の場合
+      next_next_frame = frames[frame_number + 1]
+      point += 10 + next_frame[0] + next_next_frame[0]
     else
-      # 9本以下の場合の計算
-      point += frame.sum
+      point += 10 + next_frame[0] + next_frame[1]
     end
+  elsif frame.sum == 10
+    # スペアの時の計算
+    next_frame = frames[frame_number]
+    point += 10 + next_frame[0]
+  else
+    # 9本以下の場合の計算
+    point += frame.sum
   end
   frame_number += 1
 end
 
 puts point
-
-# next_frame = frames[frame_number] の frame_number が気持ち悪い
