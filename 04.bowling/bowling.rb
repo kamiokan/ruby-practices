f80fb5e389ed520c0ef24c66203596a95893b945
kamiokan <spherenov@gmail.com>
2021-05-21 10:35:23 +0900

    ループ回数を加算していた所をEnumerator#with_indexに置き換えた

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -26,9 +26,8 @@ end
 
 # 点数計算パート
 point = 0
-frame_number = 1
-frames.each do |frame|
-  if frame_number == 10
+frames.each.with_index(1) do |frame, idx|
+  if idx == 10
     # 10フレーム目
     point += frame.sum
     next
@@ -37,23 +36,22 @@ frames.each do |frame|
   # 1フレーム目から9フレーム目までの処理
   if frame[0] == 10
     # ストライクの時の計算
-    next_frame = frames[frame_number]
-    if next_frame[0] == 10 && frame_number != 9
+    next_frame = frames[idx]
+    if next_frame[0] == 10 && idx != 9
       # 次のフレームもストライクで9フレーム目以外の場合
-      next_next_frame = frames[frame_number + 1]
+      next_next_frame = frames[idx + 1]
       point += 10 + next_frame[0] + next_next_frame[0]
     else
       point += 10 + next_frame[0] + next_frame[1]
     end
   elsif frame.sum == 10
     # スペアの時の計算
-    next_frame = frames[frame_number]
+    next_frame = frames[idx]
     point += 10 + next_frame[0]
   else
     # 9本以下の場合の計算
     point += frame.sum
   end
-  frame_number += 1
 end
 
 puts point
