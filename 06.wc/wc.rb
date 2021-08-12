03a929abcef98c4700661a4df327b567bc78fb73
kamiokan <spherenov@gmail.com>
2021-08-13 01:33:15 +0900

    -lオプション有無をunlessで整理した

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -11,47 +11,39 @@ def main
   when 0
     text = $stdin.read
     print text.lines.count.to_s.rjust(8)
-    print text.split(/\s+/).size.to_s.rjust(8)
-    puts text.bytesize.to_s.rjust(8)
+    unless options['l']
+      print text.split(/\s+/).size.to_s.rjust(8)
+      puts text.bytesize.to_s.rjust(8)
+    end
   when 1
     file_path = file_paths[0]
-    if options['l']
-      print count_lines(file_path).to_s.rjust(8)
-      puts " #{file_path}"
-    else
-      print count_lines(file_path).to_s.rjust(8)
+    print count_lines(file_path).to_s.rjust(8)
+    unless options['l']
       print count_words(file_path).to_s.rjust(8)
       print count_bytes(file_path).to_s.rjust(8)
-      puts " #{file_path}"
     end
+    puts " #{file_path}"
   else
     total_count_lines = 0
     total_count_words = 0
     total_count_bytes = 0
     file_paths.each do |file_path|
-      if options['l']
-        total_count_lines += count_lines(file_path)
-        print count_lines(file_path).to_s.rjust(8)
-        puts " #{file_path}"
-      else
-        total_count_lines += count_lines(file_path)
-        total_count_words += count_words(file_path)
-        total_count_bytes += count_bytes(file_path)
-        print count_lines(file_path).to_s.rjust(8)
+      total_count_lines += count_lines(file_path)
+      total_count_words += count_words(file_path)
+      total_count_bytes += count_bytes(file_path)
+      print count_lines(file_path).to_s.rjust(8)
+      unless options['l']
         print count_words(file_path).to_s.rjust(8)
         print count_bytes(file_path).to_s.rjust(8)
-        puts " #{file_path}"
       end
+      puts " #{file_path}"
     end
-    if options['l']
-      print total_count_lines.to_s.rjust(8)
-      puts ' total'
-    else
-      print total_count_lines.to_s.rjust(8)
+    print total_count_lines.to_s.rjust(8)
+    unless options['l']
       print total_count_words.to_s.rjust(8)
       print total_count_bytes.to_s.rjust(8)
-      puts ' total'
     end
+    puts ' total'
   end
 end
 
