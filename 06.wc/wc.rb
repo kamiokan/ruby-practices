7605f4b739f234696192e7a96526795c7cc19469
kamiokan <spherenov@gmail.com>
2021-10-15 10:57:57 +0900

    display_zero_argument()とdisplay_some_arguments()の処理内容を揃えた

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -11,31 +11,36 @@ def main
   when 0
     text = $stdin.read
     display_zero_argument(text, options)
+    puts
   when 1
     file_path = file_paths[0]
-    display_some_arguments(file_path, options)
+    text = File.read(file_path)
+    display_some_arguments(text, options)
+    puts " #{file_path}"
   else
     total_count = { lines: 0, words: 0, bytes: 0 }
     file_paths.each do |f|
-      total_count[:lines] += count_lines(f)
-      total_count[:words] += count_words(f)
-      total_count[:bytes] += count_bytes(f)
-      display_some_arguments(f, options)
+      text = File.read(f)
+      total_count[:lines] += count_lines(text)
+      total_count[:words] += count_words(text)
+      total_count[:bytes] += count_bytes(text)
+      display_some_arguments(text, options)
+      puts " #{f}"
     end
     display_total_count(total_count, options)
   end
 end
 
-def count_lines(file_path)
-  File.read(file_path).lines.count
+def count_lines(text)
+  text.lines.count
 end
 
-def count_words(file_path)
-  File.read(file_path).split(/\s+/).size
+def count_words(text)
+  text.split(/\s+/).size
 end
 
-def count_bytes(file_path)
-  File.stat(file_path).size
+def count_bytes(text)
+  text.bytesize
 end
 
 def display_zero_argument(text, options)
@@ -43,16 +48,15 @@ def display_zero_argument(text, options)
   return if options['l']
 
   print text.split(/\s+/).size.to_s.rjust(8)
-  puts text.bytesize.to_s.rjust(8)
+  print text.bytesize.to_s.rjust(8)
 end
 
-def display_some_arguments(file_path, options)
-  print count_lines(file_path).to_s.rjust(8)
-  unless options['l']
-    print count_words(file_path).to_s.rjust(8)
-    print count_bytes(file_path).to_s.rjust(8)
-  end
-  puts " #{file_path}"
+def display_some_arguments(text, options)
+  print text.lines.count.to_s.rjust(8)
+  return if options['l']
+
+  print text.split(/\s+/).size.to_s.rjust(8)
+  print text.bytesize.to_s.rjust(8)
 end
 
 def display_total_count(total_count, options)
